7f26861db778f5f6f5bfad6d4091ed08bf52566f
dancdx <1049555787@qq.com>
2018-08-16 18:01:52 +0800

    页面排版

---
+++ front/src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <h2>
+            <router-link to="/">{{title}}'s Blog</router-link>
+        </h2>
+        <div class="headerLink">
+            <router-link class="focus" to="/">首页</router-link>
+            <router-link to="/">分类</router-link>
+            <router-link to="/">标签</router-link>
+            <router-link to="/">留言板</router-link>
+            <router-link to="/">关于我</router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            title:'WangDan',
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../sass/header.scss'
+</style>
---
+++ front/src/components/nav.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="nav">
+        <div class="search-box">
+            <input type="text" placeholder="搜索内容">
+            <button></button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss">
+    @import '../sass/nav.scss';
+</style>
+
+
